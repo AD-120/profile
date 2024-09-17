b2dcfe4f50878fcee861a2bf4fe3ea1ea600b3ed
AD120 <avrahaaam@gmail.com>
2024-09-17 14:58:06 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,22 @@ let state = 0;
 let gifPlayCount = 0;
 let gifStartTime;
 let canvasSize = 400;
+let gifFrames = [];
+let currentFrame = 0;
+let gifDuration = 600; // Assume 2 seconds duration, adjust if known
 
 function preload() {
-  gif = loadImage("profile-gif_2.gif");
+  gif = loadImage("profile-gif_2.gif", imageLoaded);
   tmuna = loadImage("AM.jpg");
+}
+
+function imageLoaded() {
+  // Extract all frames from the GIF
+  let totalFrames = gif.numFrames();
+  for (let i = 0; i < totalFrames; i++) {
+    gif.setFrame(i);
+    gifFrames.push(gif.get());
+  }
 }
 
 function setup() {
@@ -21,17 +33,19 @@ function draw() {
   
   if (state === 0) {
     // Show animated GIF
-    if (gifPlayCount < 3) {
-      image(gif, -width/2, -height/2, width, height);
+    if (gifPlayCount < 3 && gifFrames.length > 0) {
+      let elapsedTime = (millis() - gifStartTime) % gifDuration;
+      currentFrame = floor(map(elapsedTime, 0, gifDuration, 0, gifFrames.length - 1));
+      image(gifFrames[currentFrame], -width/2, -height/2, width, height);
       
       // Check if the GIF has completed one loop
-      if (millis() - gifStartTime > gif.duration * 1000) {
+      if (millis() - gifStartTime > gifDuration) {
         gifPlayCount++;
         gifStartTime = millis();
       }
-    } else {
-      // Display the first frame of the GIF when it's done playing
-      image(gif.get(0, 0, gif.width, gif.height), -width/2, -height/2, width, height);
+    } else if (gifFrames.length > 0) {
+      // Display the last frame of the GIF when it's done playing
+      image(gifFrames[gifFrames.length - 1], -width/2, -height/2, width, height);
     }
   } else if (state === 1) {
     // Rotating cube
@@ -80,7 +94,7 @@ function mousePressed() {
   state = (state + 1) % 4;
   if (state === 0) {
     // Reset GIF play count when cycling back to the GIF state
-    gifPlayCount = 0;
+    gifPlayCount = 3;
     gifStartTime = millis();
   }
 }
